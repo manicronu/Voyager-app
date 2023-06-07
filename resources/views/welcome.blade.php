4284e44d3fd408ee518c88902d4cb2ccc690e656
Manicrouns <manikandan.karunakaran@ideaxecution.com>
2023-06-07 12:53:55 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to Our Website</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: #f2f2f2;
+    }
+    .container {
+      max-width: 800px;
+      margin: 0 auto;
+      padding: 20px;
+      text-align: center;
+    }
+    h1 {
+      color: #333;
+    }
+    p {
+      color: #666;
+    }
+    .button {
+      display: inline-block;
+      padding: 10px 20px;
+      background-color: #333;
+      color: #fff;
+      text-decoration: none;
+      border-radius: 4px;
+      transition: background-color 0.3s;
+    }
+    .button:hover {
+      background-color: #555;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Welcome to Our blackadmin</h1>
+    <p>Thank you for visiting our website. We are excited to have you here.</p>
+    <p>Explore our content, products, and services to find what interests you the most.</p>
+    <a href="/admin" class="button">Get Started</a>
+  </div>
+</body>
+</html>
